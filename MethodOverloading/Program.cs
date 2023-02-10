86e2e51fae6c0f62b53a43c434c83ef3eb4144ab
Jack Kessler <jack_kessler@hotmail.com>
2023-02-10 15:00:23 -0600

    Method-Overloading-Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,35 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        static decimal Add(decimal a, decimal b)
+        {
+            decimal sum = a + b;
+            return sum;
+        }
+
+        static string Add(int a, int b, bool c)
+        {
+            if (c == true)
+            {
+                int sum = a + b;
+                if (sum == 0 || sum > 1)
+                {
+                    return $"{sum} dollars";
+                }
+                else
+                {
+                    return $"{sum} dollar";
+                }
+            }
+            else
+            {
+                return "Error: This should never happen.";
+            }
+        }
     }
 }
